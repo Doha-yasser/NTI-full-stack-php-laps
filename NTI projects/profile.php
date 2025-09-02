ebc1d8d304c3e5a8482cb9adb33a17bf44a5b954
Doha-yasser <20231089@stud.fci-cu.edu.eg>
2025-09-03 01:00:21 +0300

    Add files via upload
    my first php code

---
+++ NTI projects/profile.php
@@ -0,0 +1,14 @@
+
+<?php
+    // session_start();
+    // $name = $_SESSION('username');
+    // $mail = $_SESSION('mail');
+    // $password = $_SESSION('password');
+
+    echo "UserName: ".$_POST['username'];
+    echo "<br>";
+    echo "Email: ".$_POST['mail'];
+    echo "<br>";
+    echo "password: " . $_POST['password'];
+
+?>
