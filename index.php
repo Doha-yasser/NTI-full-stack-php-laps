2266434c132d7e6741de596f4e9d3e0a3d81cf5c
Doha-yasser <183983657+Doha-yasser@users.noreply.github.com>
2025-09-03 01:06:14 +0300

    test
    Signed-off-by: Doha-yasser <183983657+Doha-yasser@users.noreply.github.com>

---
+++ index.php
@@ -0,0 +1,31 @@
+<?php
+    // commetnts:   # , // , /* */
+    // echo 'Doha'
+    
+    
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Document</title>
+    <link rel="stylesheet" href="style/style.css">
+  </head>
+  <body>
+        <div class="form">
+            <form action="profile.php" method="POST">
+                <input type="text" name="username" placeholder="UserName" required autofocus access_key="u"><br><br>
+                <input type="email" name="mail" placeholder="Email" access_key="e"><br><br>
+                <input type="password" name="password" placeholder="Password" access_key="p"><br><br>
+                <input type="submit" class="btn">
+            </form>
+        </div>
+        
+    <script></script>
+  </body>
+</html>
+
